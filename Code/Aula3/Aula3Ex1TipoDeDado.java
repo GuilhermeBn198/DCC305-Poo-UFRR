a1922281bcdec4e6b679d95c7553707044a1ddf5
Guilherme Bernardo <bguilherme51@gmail.com>
2021-03-25 21:12:16 -0400

    inicio aula 3

---
+++ Code/Aula3/Aula3Ex1TipoDeDado.java
@@ -0,0 +1,10 @@
+package Code.Aula3;
+
+public class Aula3Ex1TipoDeDado {
+    int peso = 100;
+    int peso2 = peso + 1;
+    int x = 15;
+    x = x+1;
+    char sexo;
+    boolean certo;
+}
--- Code/Aula3/aaa.java
+++
@@ -1,5 +0,0 @@
-package Code.Aula3;
-
-public class aaa {
-    
-}
