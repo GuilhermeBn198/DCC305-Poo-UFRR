ea58fa9b9b234ff29a3e22a780c7c424d2d46025
Guilherme Bernardo <bguilherme51@gmail.com>
2021-03-28 18:55:06 -0400

    Questao 8 e 10 concluidas e com isso a atividade para casa da aula 03 finalmente foi concluída
    amem

---
+++ Code/Aula3/ExercicioCasaAula03/Questao10.java
@@ -0,0 +1,12 @@
+package Code.Aula3.ExercicioCasaAula03;
+
+public class Questao10 {
+    public static void main(String[] args) {
+        for (int i = 1; i<10; i++){
+			for (int j = 1; j<=i; j++){
+				System.out.print(i*j + " ");
+			}
+		    System.out.println(""); // muda de linha   
+		}
+    }
+}
--- Code/Aula3/ExercicioCasaAula03/Questao8.java
+++ Code/Aula3/ExercicioCasaAula03/Questao8.java
@@ -2,16 +2,23 @@ package Code.Aula3.ExercicioCasaAula03;
 import java.util.Scanner;
 
 public class Questao8 {
-    public static float test(int x) {
-        
+    public static int test(int x) {
+        if ((x%2)==0) 
+            x = x/2;
+        else
+            x = 3 * x + 1;
+        return x;
     }      
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
-        System.out.println("digite um valor para x que não seja 1:");
-        float x = scan.nextFloat();
+        System.out.println("digite um valor para x:");
+        int x = scan.nextInt();
 
+        for (int i = x; i >= 5 ; i--) {
+            x = test(x);
+            System.out.print(x+"  ");
+        }
 
     }   
 }
