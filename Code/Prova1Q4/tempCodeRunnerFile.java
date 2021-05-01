00a918cd7bdd7937598023f52194575753853c0b
Paulo-player <silverpaulo7@hotmail.com>
2021-05-01 02:47:14 -0400

    Q4 Iniciada, ler comentários na classe Passo
    Criado thread por meio de herança de Thread e instanciação dos objetos do tipo Passo, que herda Thread
    
    Instanciado primeiro rato e testado o print do labirinto

---
+++ Code/Prova1Q4/tempCodeRunnerFile.java
@@ -0,0 +1,9 @@
+public static void printarLabirinto(){
+        for (int i=0;i<9;i++){
+            for(int j=0;j<33;j++){
+                System.out.printf("%c",labirintor[i][j]);
+            }
+            System.out.println("");
+        }
+
+    }
